cd4e68329b4bc9ea9ecfa1f40f5ab4bad6e20a92
Fidel Vera <fevc1988@gmail.com>
2024-02-04 15:12:36 -0300

    penguins updated

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -4,6 +4,23 @@ import altair as alt
 import seaborn as sns
 
 st.title("Palmer's Penguins")
-# Import the dataset
-penguins_df = pd.read_csv('penguins.csv')
-st.write(penguins_df.head())
+st.markdown("Use this Streamlit app to make your own scatterplot about penguins!")
+
+penguin_file = st.file_uploader("Select Your Local Penguins CSV (default provided)")
+if penguin_file is not None:
+    penguins_df = pd.read_csv(penguin_file)
+else:
+    penguins_df = pd.read_csv("penguins.csv")
+
+selected_x_var = st.selectbox('What do you want the x variable to be?', ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g'])
+
+alt_chart = (alt.Chart(penguins_df, title=f"Scatterplot of Palmer's Penguins")
+    .mark_circle()
+    .encode(
+        x=selected_x_var,
+        y=selected_y_var,
+        color = "species"
+    ).interactive())
+
+st.altair_chart(alt_chart, use_container_width=True)
